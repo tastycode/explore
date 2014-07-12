7d03e68e544fc68782a1434cc87f41cf80b3c2bd
tastycode <thomas@puddle.com>
2014-07-12 16:02:23 -0700

    Real matching

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     if current_user
-      @matches = User.all#current_user.find_matches
+      @matches = current_user.find_matches
     end
   end
 end
