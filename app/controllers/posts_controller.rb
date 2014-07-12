fd7a333576fadce9fddab6033a8cb05a3da24603
Sierra <sierrabrown808@gmail.com>
2014-07-12 15:56:03 -0700

    fix css in user page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
   
   def create
     @post = Post.new(post_params)
+    @post.user = current_user
     if @post.save
       redirect_to user_url(@post.recipient_id)
     else
