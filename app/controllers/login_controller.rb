91120612f303c3f9897cd072d287c3a82566579f
tastycode <thomas@puddle.com>
2014-07-12 01:21:17 -0700

    facepalm

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,7 +1,6 @@
 class LoginController < ApplicationController
 
   def store_and_redirect
-    binding.pry
     cookies[:ability] = params[:ability]
     cookies[:wants] = params[:wants]
     redirect_to user_omniauth_authorize_path(:facebook)
